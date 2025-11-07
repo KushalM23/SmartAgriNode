40dd80621aea12faf6c3b4a8b996635b2bd84c37
Kushal <kushalll.study@gmail.com>
2025-11-07 14:16:52 +0530

    added frontend url

--- backend/main.py
+++ backend/main.py
@@ -50,7 +50,7 @@ app.add_middleware(
         "http://127.0.0.1:5175",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://smartagrinode-backend.onrender.com"
+        "https://smart-agri-node.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
